b85d52477acbc784a60a152407e4cf73d4c7019d
AndreyBadan <andreibadanin@rambler.ru>
2019-06-14 01:12:32 +0300

    MealServlet implementation

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,45 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.DBUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class MealServlet extends HttpServlet {
+    private static final Logger log = getLogger(MealServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.debug("redirect to meals");
+
+        req.setAttribute("listMealTo", convertToMealTo(DBUtils.queryMeal(), 2000));
+        req.getRequestDispatcher("/meals.jsp").forward(req, resp);
+
+    }
+
+    private static List<MealTo> convertToMealTo(List<Meal> meals, int caloriesPerDay) {
+        Map<LocalDate, Integer> caloriesSumByDate = meals.stream()
+                .collect(Collectors.groupingBy(Meal::getDate, Collectors.summingInt(Meal::getCalories)));
+
+        return meals.stream()
+                .map(meal -> createWithExcess(meal, caloriesSumByDate.get(meal.getDate()) > caloriesPerDay))
+                .collect(Collectors.toList());
+    }
+
+    private static MealTo createWithExcess(Meal meal, boolean excess) {
+        return new MealTo(meal.getDateTime(), meal.getDescription(), meal.getCalories(), excess);
+    }
+}
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,27 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
+
+<html>
+<head>
+    <title>Meals</title>
+</head>
+<body>
+    <h3><a href="index.html">Home</a></h3>
+    <hr>
+    <table border="1">
+        <c:forEach var="mealTo" items="${listMealTo}">
+            <fmt:parseDate value="${mealTo.dateTime}" pattern="yyyy-MM-dd'T'HH:mm" var="parsedDateTime" type="both" />
+            <fmt:formatDate pattern="dd.MM.yyyy HH:mm" value="${parsedDateTime}" var="parsedDate" />
+            <tr bgcolor="${mealTo.excess ? "red" : "green"}">
+                <td>${parsedDate}</td>
+                <td>${mealTo.description}</td>
+                <td>${mealTo.calories}</td>
+            </tr>
+        </c:forEach>
+    </table>
+</body>
+</html>
